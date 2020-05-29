78deb1b80218e09f6d87f6adc747c63d3ad63fb8
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-05-29 12:31:25 +0530

    Update team.cpp

--- team.cpp
+++ team.cpp
@@ -1,11 +1,7 @@
 #include "team.h"		// "player.h", <string>, <vector>
 
 Team::Team() {
-  
   totalRunsScored = 0;
-
-	wicketsLost = 0;
-
-	totalBallsBowled = 0;
-
+  wicketsLost = 0;
+  totalBallsBowled = 0;
 }
