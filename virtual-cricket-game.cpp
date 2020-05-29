b43e994b950bd1dc3aacadf010eb85cc57136a51
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-05-29 12:35:52 +0530

    Update virtual-cricket-game.cpp

--- virtual-cricket-game.cpp
+++ virtual-cricket-game.cpp
@@ -1,61 +1,28 @@
 #include "game.h"
-
 using namespace std;
 
-
-
-
-
 int main() {
-
-
-
+	
 	Game game;
-
 	game.welcome();
-
-
+	cout << "\nPress Enter to continue";
+	getchar();
+        game.showAllPlayers();
 
 	cout << "\nPress Enter to continue";
-
 	getchar();
-
-
-
-    game.showAllPlayers();
-
-
-
-	cout << "\nPress Enter to continue";
-
-	getchar();
-
-
 
 	game.selectPlayers();
 
 	game.showTeamPlayers();
 
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
 
+        cout << "\nPress Enter to toss";
 
-    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        getchar();
 
-
-
-    cout << "\nPress Enter to toss";
-
-    getchar();
-
-
-
-    game.toss();
-
-
-
-    game.startFirstInnings();
-
-
-
-	return 0;
-
+        game.toss();
+        game.startFirstInnings();
+        return 0 
 }
