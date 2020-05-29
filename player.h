34ad000d37e933566305378c534bd1825de11a8f
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-05-29 12:30:20 +0530

    Update player.h

--- player.h
+++ player.h
@@ -1,29 +1,13 @@
 #include <string>
 
-
-
-
-
 class Player {
-
-
-
 	public:
-
 		Player();
-
 		std::string name;
-
 		int id;
-
 		int runsScored;
-
 		int ballsPlayed;
-
 		int ballsBowled;
-
 		int runsGiven;
-
 		int wicketsTaken;
-
 };
