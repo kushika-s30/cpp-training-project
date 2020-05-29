30771e8c4cd981ba17f4df3fd99feb6d6106b67f
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-05-29 11:54:02 +0530

    Create player.h

---
+++ player.h
@@ -0,0 +1,29 @@
+#include <string>
+
+
+
+
+
+class Player {
+
+
+
+	public:
+
+		Player();
+
+		std::string name;
+
+		int id;
+
+		int runsScored;
+
+		int ballsPlayed;
+
+		int ballsBowled;
+
+		int runsGiven;
+
+		int wicketsTaken;
+
+};
